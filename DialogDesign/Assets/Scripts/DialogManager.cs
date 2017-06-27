e043a821377c0e80af50d7a69049bc43f9862153
Rahul Kumar <rkrahul1611@gmail.com>
2017-06-28 00:10:41 +0530

    Custom Dynamic Animation DialogUI

---
+++ DialogDesign/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour {
+	public GameObject DialogObject;
+	public static DialogManager instance = null;
+
+	void Awake(){
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Show(){
+		Debug.Log("DialogShow");
+		//DialogObject.GetComponent<EasyTween> ().OpenCloseObjectAnimation ();
+		DialogObject.GetComponent<Dialog>().ShowDialog();
+	}
+}
---
+++ DialogDesign/Assets/Scripts/RotatingBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBackground : MonoBehaviour {
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, Time.deltaTime * speed);
+	}
+}
